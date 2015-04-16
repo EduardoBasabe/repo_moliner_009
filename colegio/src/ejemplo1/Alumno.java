984adbe710fefb1a405908fc6376049ee707911b
EDU <EDU@hotmail.com>
2015-04-16 17:16:22 +0200

    cambios que hemos realizado
    hemos añadido la clase Profesor
    hemos añadido el campo apellido en la clase Alumno
    hemos creado los ficheros fichero.txt y ficheroDos.txt
    (estos ficheros seran ignorados)

--- colegio/src/ejemplo1/Alumno.java
+++ colegio/src/ejemplo1/Alumno.java
@@ -1,16 +1,34 @@
 package ejemplo1;
+
 public class Alumno {
 /* Constructor */
 protected String nombre;
 protected int edad;
+protected String apellido;
+
+//este es el constructor con nombre y edad
+
 public Alumno(String a, int b){
 nombre = a;
 edad = b;
 }
+
+public Alumno(String a, int b, String ap){
+nombre = a;
+edad = b;
+apellido = ap;
+}
+
 public int getEdad() {
 return edad;
 }
+
 public String getNombre() {
 return nombre;
 }
+
+public String getApellido() {
+	return apellido;
+}
+
 }
---
+++ colegio/src/ejemplo1/Profesor.java
@@ -0,0 +1,25 @@
+package ejemplo1;
+
+public class Profesor {
+
+	private String nomProfesor;
+	private String ApeProfesor;
+	
+	public String getNomProfesor() {
+		return nomProfesor;
+	}
+	
+	public String getApeProfesor() {
+		return ApeProfesor;
+	}
+
+	public Profesor(String nomProfesor, String apeProfesor) {
+		super();
+		this.nomProfesor = nomProfesor;
+		this.ApeProfesor = apeProfesor;
+	}
+	
+	
+	
+	
+}
